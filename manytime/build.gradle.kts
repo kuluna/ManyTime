3e7c88e89b9d421bf6f4b796ce1302ba85cab19a
Ryota Sakaguchi <mailkuluna@gmail.com>
2020-04-21 15:52:27 +0900

    update

--- manytime/build.gradle.kts
+++ manytime/build.gradle.kts
@@ -56,7 +56,7 @@ configure<PublishExtension> {
     userOrg = "kuluna"
     groupId = "jp.kuluna"
     artifactId = "manytime"
-    publishVersion = "0.0.5"
+    publishVersion = "0.0.6"
     desc = "Start time and end time show/input view."
     website = "https://github.com/kuluna/ManyTime"
 
